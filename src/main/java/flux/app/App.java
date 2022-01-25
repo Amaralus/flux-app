053f9e64001133a24f8088015ba2b8a578dce389
Evgenii Dolgov <river100@mail.ru>
2022-01-25 17:43:26 +0300

    initial commit

---
+++ src/main/java/flux/app/App.java
@@ -0,0 +1,12 @@
+package flux.app;
+
+public class App {
+
+    public String greeting() {
+        return "hello mtfk";
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hello gradle");
+    }
+}
